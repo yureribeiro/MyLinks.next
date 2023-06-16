780117a8d53761f147373cf6e3a273c9de0dc415
Mac <mac@MacBook-Pro-de-Mac.local>
2023-06-16 17:00:09 -0300

    add pagina desksetup, estilo e navegaçao

--- app/desksetup/page.jsx
+++ app/desksetup/page.jsx
@@ -1,7 +1,64 @@
+import Link from "next/link"
+import Image from "next/image"
+import styles from "./desksetup.module.css"
+import ArrowLeft from "../../public/arrow-fat-left.svg"
+
 export default function DeskSetup() {
     return (
-        <div>
-            <h1>meu setup pagina.</h1>
+        <div className={styles.container}>
+            <div className={styles.backContent}>
+                  <Link 
+                    href={'/'} 
+                    className={styles.back}
+                    > 
+                    <Image 
+                    src={ArrowLeft}
+                    className={styles.imageArrowLeft}
+                    alt="seta de voltar"
+                    />
+                    Voltar
+                  </Link>
+            </div>
+          
+          <h1 className={styles.title}>meu setup pagina.</h1>
+            <nav className={styles.navLinks}>
+                <Link 
+                  href={'#'}
+                  className={styles.link}
+                  >
+                    Produto 1
+                </Link>
+                <Link 
+                  href={'#'}
+                  className={styles.link}
+                  >
+                    Produto 2
+                </Link>
+                <Link 
+                  href={'#'}
+                  className={styles.link}
+                  >
+                    Produto 3
+                </Link>
+                <Link 
+                  href={'#'}
+                  className={styles.link}
+                  >
+                    Produto 4
+                </Link>
+                <Link 
+                  href={'#'}
+                  className={styles.link}
+                  >
+                    Produto 5
+                </Link>
+                <Link 
+                  href={'#'}
+                  className={styles.link}
+                  >
+                    Produto 6
+                </Link>
+            </nav>  
         </div>
     )
 }
